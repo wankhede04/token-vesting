7fbfdd02cf95f24a1f6710b4dac78eeb6495ae30
wankhede04 <wankhedevijay04@gmail.com>
2023-02-22 19:35:57 +0530

    Setup hardhat configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.POLYGON_TESTNET_ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      throwOnTransactionFailures: true,
+      loggingEnabled: true,
+      gasMultiplier: 1.5
+    }
+  },
+  etherscan: {
+    apiKey: process.env.EXPLORER_API_KEY,
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  }
+};
+
+export default config;
