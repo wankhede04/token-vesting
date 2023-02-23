782295db4b7326bb72178507d98daa3fd258df02
wankhede04 <wankhedevijay04@gmail.com>
2023-02-23 11:59:53 +0530

    Correct import path of IERC20 methodata

--- contracts/EquityVesting.sol
+++ contracts/EquityVesting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import { IERC20Metadata } from "node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract EquityVesting {
     struct Equity {
