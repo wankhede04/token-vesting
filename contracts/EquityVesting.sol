3844b6ba1e4acb9ed609d11e9cf83f73343711b1
wankhede04 <wankhedevijay04@gmail.com>
2023-02-23 13:22:21 +0530

    Add method to claim equity

--- contracts/EquityVesting.sol
+++ contracts/EquityVesting.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-contract EquityVesting {
+contract EquityVesting is AccessControl {
     struct Equity {
         uint256 vestingAmount;
         uint256 percentRelease;
         uint256 distributionInterval;
     }
-
     struct Employee {
         bytes4 class;
         uint256 vestingCliff;
@@ -17,7 +17,8 @@ contract EquityVesting {
     }
 
     bytes4 public constant CXO = bytes4(keccak256(abi.encodePacked("CXO")));
-    bytes4 public constant SENIOR_MANAGER = bytes4(keccak256(abi.encodePacked("SENIOR_MANAGER")));
+    bytes4 public constant SENIOR_MANAGER =
+        bytes4(keccak256(abi.encodePacked("SENIOR_MANAGER")));
     bytes4 public constant OTHER = bytes4(keccak256(abi.encodePacked("OTHER")));
     uint256 public constant PERCENT_BASE = 10000;
 
@@ -25,28 +26,22 @@ contract EquityVesting {
     mapping(address => Employee) public employeeDetails;
     IERC20Metadata public equityToken;
 
-    constructor(IERC20Metadata _equityToken) {
+    constructor(IERC20Metadata _equityToken, address _admin) {
         equityToken = _equityToken;
-        _updateEquity(Equity(
-            1000,
-            2500,
-            365 days
-        ), CXO);
-        _updateEquity(Equity(
-            800,
-            2500,
-            365 days
-        ), SENIOR_MANAGER);
-        _updateEquity(Equity(
-            400,
-            5000,
-            365 days
-        ), OTHER);
+        _updateEquity(Equity(1000, 2500, 365 days), CXO);
+        _updateEquity(Equity(800, 2500, 365 days), SENIOR_MANAGER);
+        _updateEquity(Equity(400, 5000, 365 days), OTHER);
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
 
-    function addEmployees(address[] memory _recipients, bytes4[] memory _recipientClass) external {
-        // TODO: add access to admin
-        require(_recipients.length == _recipientClass.length, "EquityVesting: invalid array data");
+    function addEmployees(
+        address[] memory _recipients,
+        bytes4[] memory _recipientClass
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            _recipients.length == _recipientClass.length,
+            "EquityVesting: invalid array data"
+        );
         uint256 totalRecipients = _recipients.length;
         uint256 currentTimestamp = block.timestamp;
         uint256 year = 365 days;
@@ -60,17 +55,39 @@ contract EquityVesting {
         // TODO: add event
     }
 
-    function getEquityToClaim(address recipient) external view returns (uint256 amount) {
+    function claimEquity() external returns (uint256 amount) {
+        address recipient = _msgSender();
+        uint256 interval;
+        (amount, interval) = getEquityToClaim(recipient);
+        require(amount != 0, "EquityVesting: zero amount to claim");
+
+        Employee storage employee = employeeDetails[recipient];
+        employee.lastUpdated += interval;
+        equityToken.transfer(recipient, amount);
+    }
+
+    function getEquityToClaim(address recipient)
+        public
+        view
+        returns (uint256 amount, uint256 interval)
+    {
         uint256 currentTimestamp = block.timestamp;
         Employee memory employee = employeeDetails[recipient];
         // If Employee.vestingCliff == Equity.distributionInterval, this check is irrelevant.
         if (currentTimestamp < employee.vestingCliff) amount = 0;
 
         Equity memory equity = equityByClass[employee.class];
-        if (currentTimestamp > equity.distributionInterval + employee.lastUpdated) {
-            uint256 unclaimedPeriod = (currentTimestamp - employee.lastUpdated) / equity.distributionInterval;
-            amount = (equity.vestingAmount * (equity.percentRelease * unclaimedPeriod)) / PERCENT_BASE;
-            // in setter, employee.lastUpdated += employee.vestingCliff;
+        if (
+            currentTimestamp >
+            equity.distributionInterval + employee.lastUpdated
+        ) {
+            uint256 unclaimedPeriod = (currentTimestamp -
+                employee.lastUpdated) / equity.distributionInterval;
+            amount =
+                (equity.vestingAmount *
+                    (equity.percentRelease * unclaimedPeriod)) /
+                PERCENT_BASE;
+            interval = equity.distributionInterval;
         }
     }
 
