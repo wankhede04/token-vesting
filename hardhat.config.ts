edc1033f1b72e0e5078330ed2abcc14e6468ef67
wankhede04 <wankhedevijay04@gmail.com>
2023-02-23 11:57:59 +0530

    Change solidity pragma to 0.8.17

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.18",
+        version: "0.8.17",
         settings: {
           optimizer: {
             enabled: true,
